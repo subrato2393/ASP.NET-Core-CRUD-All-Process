8d979d90d14c1f2c956e2a1ec7c237aea46dea15
Subrato Ghosh <subrato2393@gmail.com>
2021-03-18 23:14:32 +0600

    Creating another inventory app for following best practise

---
+++ InventoryAppBasicCRUD/InventoryApp/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public virtual Category Category { get; set; }
+        public  int CategoryId { get; set; } 
+    }
+}
