4209a5ac8c69f508b390ccf03c159e12abcdf94c
Subrato Ghosh <subrato2393@gmail.com>
2021-09-08 18:27:12 +0600

    Model bind using jquery master details

---
+++ JqueryMasterDetailsAsp.NetCoreApp/JqueryMasterDetailsAsp.NetCoreApp/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using JqueryMasterDetailsAsp.NetCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JqueryMasterDetailsAsp.NetCoreApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult CreateOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder(Order order)
+        {
+            return View();
+        }
+    }
+}
